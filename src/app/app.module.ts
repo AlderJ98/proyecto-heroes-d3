e746a4460048f1434468bc1cf82f71f20490acbe
AlderJ98 <alderfigueroa@hotmail.com>
2022-07-27 20:20:55 -0500

    navabars link router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { EjemplosPipesComponent } from './ejemplos-pipes/ejemplos-pipes.component';
+import { RouterModule } from '@angular/router';
 
 // decorador empieza con @ es una funcion comun y corriente
 @NgModule({
@@ -20,6 +22,8 @@ import { EjemplosPipesComponent } from './ejemplos-pipes/ejemplos-pipes.componen
   // importar las librerias
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
